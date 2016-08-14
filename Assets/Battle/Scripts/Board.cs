6dda9fedb5de89d74e0792032b3f1db2365cf9a5
WYVERN\Checkmate <dgeisler50@gmail.com>
2016-08-14 07:47:55 -0600

    Board made, simple pathfinding working

---
+++ Assets/Battle/Scripts/Board.cs
@@ -0,0 +1,208 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Board : MonoBehaviour {
+
+    [SerializeField]
+    private int r;
+    [SerializeField]
+    private int c;
+    [SerializeField]
+    private float tl;
+    [SerializeField]
+    private Tile empty;
+    [SerializeField]
+    private Tile rough;
+
+    private Tile[,] board; //A list of all tiles on the board ordered by row and column
+    private LinkedList<Tile> softShaded; //A list of softly shaded tiles
+    private LinkedList<Tile> hardShaded; //A list of hard shaded tiles
+
+	// Use this for initialization
+	void Start () {
+        CreateBoard(r, c, tl);
+        softShaded = new LinkedList<Tile>();
+        hardShaded = new LinkedList<Tile>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public Tile getTile(int row, int col) {
+        return board[row, col];
+    }
+
+    // Creates a board of hexagons with 'rows', 'cols' columns, and tiles 'tileLength' apart
+    // Note that tileLength implicitely gives the vertical distance between tiles
+    public void CreateBoard(int rows, int cols, float tileLength) {
+        board = new Tile[rows, cols];
+        float totalLength = tileLength * (cols-1);
+        float totalHeight = tileLength * (rows-1);
+        System.Random myRand = new System.Random();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                int adjCount = 6;
+                if (i == 0 || i == rows - 1)
+                    adjCount -= 2;
+                if (j == 0 || j == cols - 1) {
+                    adjCount -= 1;
+                    if (i % 2 == 0 && j == 0)
+                        adjCount -= 2;
+                    else if (i % 2 == 1 && j == cols - 1)
+                        adjCount -= 2;
+                }
+                if (adjCount == 1)
+                    adjCount = 2;
+
+                //Note that the x becomes more positive while the y becomes more negative as j/i increase
+                Vector2 pos = new Vector2((j * tileLength) - (totalLength / 2) - (tileLength / 4), (totalHeight / 2) - (i * tileLength));
+                if (i % 2 == 1)
+                    pos.x += tileLength / 2;
+                Tile currentTile;
+                if (myRand.Next(0, 100) > 70)
+                    currentTile = (Tile) GameObject.Instantiate(rough, pos, Quaternion.identity);
+                else
+                    currentTile = (Tile)GameObject.Instantiate(empty, pos, Quaternion.identity);
+                currentTile.Initialize(this, adjCount);
+
+                if (j > 0)
+                    currentTile.addAdjacent(board[i, j - 1]);
+                if (i > 0) {
+                    currentTile.addAdjacent(board[i - 1, j]);
+                    if (i % 2 == 0 && j != 0)
+                        currentTile.addAdjacent(board[i - 1, j - 1]);
+                    if (i % 2 == 1 && j != cols - 1)
+                        currentTile.addAdjacent(board[i - 1, j + 1]);
+                }   
+
+                board[i, j] = currentTile;
+            }
+        }
+    }
+
+    //Removes shading from all tiles in the soft list and empties the list
+    //If not soft, also removes shading/selectability from all tiles in the hard list and empties the list
+    public void removeShade(bool soft) {
+        if (!soft) {
+            foreach (Tile t in hardShaded)
+                t.removeShade(false);
+            hardShaded.Clear();
+        }
+        foreach (Tile t in softShaded)
+            t.removeShade(true);
+        softShaded.Clear();
+    }
+
+    //Shades all tiles within the given 'distance' from the 'start' tile
+    //If not soft, shades the tiles hard and adds selectability
+    public void shade(Tile start, int distance, bool soft) {
+        removeShade(soft);
+        getLL(soft).AddFirst(start);
+        LLQueue tileQueue = new LLQueue();
+        foreach (Tile t in start.getAllowedAdj(distance, soft)) {
+            t.setDist(t.getCost(), soft);
+            tileQueue.insert(t, soft);
+        }
+        while (!tileQueue.empty()) {
+            Tile next = tileQueue.pop();
+            next.shade(soft);
+            getLL(soft).AddFirst(next);
+            foreach(Tile t in next.getAllowedAdj(distance, soft)) {
+                if (getLL(soft).Contains(t))
+                    continue;
+                if (t.getDist(soft) != 0 && t.getDist(soft) < next.getDist(soft) + t.getCost())
+                    continue;
+                if (t.getDist(soft) > 0)
+                    tileQueue.remove(t);
+                t.setDist(next.getDist(soft) + t.getCost(), soft);
+                tileQueue.insert(t, soft);
+            }
+        }
+    }
+
+    private LinkedList<Tile> getLL(bool soft) {
+        if (soft)
+            return softShaded;
+        return hardShaded;
+    }
+
+    private class LLQueue
+    {
+        private TileNode head;
+
+        public LLQueue() {
+            head = null;
+        }
+
+        public bool empty() {
+            return head == null;
+        }
+
+        public Tile pop() {
+            Tile toReturn = head.item;
+            head = head.next;
+            return toReturn;
+        }
+
+        public void insert(Tile item, bool soft) {
+            if (head == null) {
+                head = new TileNode(item);
+                return;
+            }
+            TileNode prev = null;
+            TileNode current = head;
+            while (current != null && current.item.getDist(soft) <= item.getDist(soft)) {
+                prev = current;
+                current = current.next;
+            }
+            if (prev == null)
+                head = new TileNode(item, current);
+            else
+                prev.next = new TileNode(item, current);
+        }
+
+        public void remove(Tile item) {
+            TileNode prev = null;
+            TileNode current = head;
+            while (current.item != item) {
+                prev = current;
+                current = current.next;
+            }
+            if (prev == null)
+                head = current.next;
+            else
+                prev.next = current.next;
+        }
+
+        public override string ToString() {
+            String toReturn = "[";
+            TileNode current = head;
+            while (current != null) {
+                toReturn += current.item + " ";
+                current = current.next;
+            }
+            return toReturn.TrimEnd() + "]";
+        }
+    }
+
+    private class TileNode
+    {
+        public Tile item;
+        public TileNode next;
+
+        public TileNode(Tile item) {
+            this.item = item;
+            this.next = null;
+        }
+
+        public TileNode(Tile item, TileNode next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+}
---
+++ Assets/Battle/Scripts/Tile.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Tile : MonoBehaviour {
+
+    public GameObject unitContained;
+
+    [SerializeField]
+    private int cost;
+    private Board holder;
+    private bool selectable;
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
+    private int hardDist;
+    private int softDist;
+
+    //A list of tiles adjacent to this tile
+    //Sorted on creation
+    private Tile[] adjacentTiles;
+    private int currentLength;
+
+	// Use this for initialization
+	void Start () {
+        unitContained = null;
+        selectable = false;
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override String ToString()
+    {
+        return "(" + this.transform.position.x + ", " + this.transform.position.y + ")";
+    }
+
+    // Creates a tile which can have up to 'adjacentCount' tiles adjacent
+    public void Initialize(Board holder, int adjacentCount) {
+        this.adjacentTiles = new Tile[adjacentCount];
+        this.holder = holder;
+        currentLength = 0;
+    }
+
+    //Adds the given tile to the list of adjacent tiles in sorted order
+    //Also adds this tile to the given tile by the same call
+    public void addAdjacent(Tile t) {
+        addAdjacent(t, true);
+    }
+
+    //Adds the given tile to the list of adjacent tiles in sorted order
+    //Can decide whether to add this tile to the given tile
+    public void addAdjacent(Tile t, bool addEquivalent) {
+        currentLength++;
+        if (currentLength > adjacentTiles.Length)
+            throw new IndexOutOfRangeException("Attempting to add " + currentLength + " adjacent tiles to the tile " + this.ToString()
+                + "which can only hold " + adjacentTiles.Length + " tiles");
+
+        for (int i = 0; i < currentLength-1; i++) {
+            if (adjacentTiles[i].getCost() > t.getCost()) {
+                //Insert t into list
+                for (int j = currentLength-1; j > i; j--)
+                    adjacentTiles[j] = adjacentTiles[j - 1];
+                adjacentTiles[i] = t;
+                if (addEquivalent)
+                    t.addAdjacent(this, false);
+                return;
+            }
+        }
+
+        adjacentTiles[currentLength-1] = t;
+        if (addEquivalent)
+            t.addAdjacent(this, false);
+    }
+
+    public int getCost() {
+        return this.cost;
+    }
+
+    public void shade(bool soft) {
+        if (!soft)
+            selectable = true;
+        Color temp = spriteRenderer.color;
+        if (soft)
+            spriteRenderer.color = new Color(temp.r * .75f, temp.g * .75f, temp.b * .75f);
+        else
+            spriteRenderer.color = new Color(temp.r * .5f, temp.g * .5f, temp.b * .5f);
+    }
+
+    //Removes shade and set dist for either soft or hard
+    public void removeShade(bool soft) {
+        if (!soft)
+            selectable = false;
+        resetDist(soft);
+        spriteRenderer.color = originalColor;
+        if (selectable)
+            this.shade(false);
+    }
+
+    //Returns the list of adjacent tiles.  Note that this list is sorted
+    public Tile[] getAdjacent() {
+        return adjacentTiles;
+    }
+
+    public Tile[] getAllowedAdj(int distance, bool soft) {
+        int index = 0;
+        for (; index < adjacentTiles.Length; index++)
+            if (this.getDist(soft) + adjacentTiles[index].getCost() > distance)
+                break;
+        index -= 1;
+        Tile[] toReturn = new Tile[index + 1];
+        for (int i = 0; i <= index; i++) {
+            toReturn[i] = adjacentTiles[i];
+        }
+        return toReturn;
+    }
+
+    //Resets distances on this tile
+    //If soft, doesn't reset hard distances
+    public void resetDist(bool soft) {
+        if (!soft)
+            hardDist = 0;
+        softDist = 0;
+    }
+
+    //Sets the distance appropriately
+    public void setDist(int value, bool soft) {
+        if (soft)
+            softDist = value;
+        else
+            hardDist = value;
+    }
+
+    //Gets the appropriate distance
+    public int getDist(bool soft) {
+        return soft ? softDist : hardDist;
+    }
+
+    void OnMouseOver() {
+        if (Input.GetMouseButtonDown(0))
+            holder.shade(this, 3, true);
+        if (Input.GetMouseButtonDown(1))
+            holder.shade(this, 3, false);
+    }
+}
